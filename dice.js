87d65445f9c37549ee5111d1d070c50ffbdf0228
Marsha Hall <mphall77@gmail.com>
2021-02-28 12:35:54 -0500

    print history

--- dice.js
+++ dice.js
@@ -1,4 +1,4 @@
-console.log("Hey you!");
+console.log("I am awesome!");
 
 diceArr = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"];
 diceVal = ["1", "2", "3", "4", "5", "6"];
@@ -14,8 +14,8 @@ let sum = 0;
 // HISTORY SELECTORS
 const ul = document.querySelector("ul");
 let historyLog;
+let HistoryLog2;
 let clicks = 0;
-
 
 // FUNCTIONS
 const getDice = (event) => {
@@ -23,40 +23,45 @@ const getDice = (event) => {
 
 	rolls = userInput.valueAsNumber;
 	for (let i = 0; i < rolls; i++) {
-        let num = Math.floor(Math.random() * 6) + 1; 
-        diceList += diceArr[num]; 
-        // how do I display the dice image?
-        // needs to go into the html w/o the quotes
-        // convertDice()
-        sum += Number(diceVal[num]);
-    }
-    pDice.textContent = diceList; //need to store this before the second click
-    pSum.textContent = sum;
-    userInput.value = "";
+		let num = Math.floor(Math.random() * 5) + 1;
+		diceList += diceArr[num];
+		// how do I display the dice image?
+		// needs to go into the html w/o the quotes
+		// convertDice()
+		sum += Number(diceVal[num]);
+	}
+	pDice.textContent = diceList; //need to store this before the second click
+	pSum.textContent = sum;
+	userInput.value = "";
 
-    const createHistory = (historyLog2) => {
-        // event.preventDefault();
-        const li = document.createElement("li");
-        li.textContent = historyLog2;
-        ul.appendChild(li);
-        historyLog = "";
-}
-
-    clicks++;
-    if (clicks === 1) {
-        historyLog = `${pDice.textContent} = ${sum}`;
+    const reset = () => {
         historyLog2 = historyLog;
-        historyLog = "";
-    } 
-    if (clicks > 1) {
-        // historyLog2 = historyLog;
-        // historyLog = "";
-        createHistory(historyLog2);
-    } 
+		diceList = "";
+		sum = 0;
+		historyLog = "";
+	};
 
-    
-        
-        
+	const createHistory = () => {
+		// event.preventDefault();
+		const li = document.createElement("li");
+		li.textContent = historyLog2;
+		ul.appendChild(li);
+		// historyLog = "";
+        reset();
+	};
+
+	clicks++;
+	// on click 1 save the (diceList = sum) values
+	// reset diceList
+	// pass current li  to historyLog
+	if (clicks === 1) {
+		historyLog = `${pDice.textContent} = ${sum}`;
+        reset();
+	}
+    if (clicks > 1) {
+        historyLog = `${pDice.textContent} = ${sum}`;
+		createHistory();
+	}
 };
 // get the user input value as a number
 // loop userInput times to generate the dice $ value
