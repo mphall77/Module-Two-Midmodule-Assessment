53dc61bcf4561ccb5f09192d49fcc956ecefec17
Marsha Hall <mphall77@gmail.com>
2021-02-28 10:45:06 -0500

    add js skeleton

--- dice.js
+++ dice.js
@@ -1,1 +1,28 @@
-console.log("Hey you!")
+console.log("Hey you!");
+
+// SELECTORS
+const userInput = document.querySelector("input");
+const diceBtn = document.querySelector("button");
+const pSum = document.querySelector("#sum-para");
+const pDice = document.querySelector("#dice");
+let diceList = "";
+
+
+// FUNCTIONS
+
+// get the user input value as a number
+// loop userInput times to generate the dice
+// add to string ==> need a variable 
+// reset input to 0
+
+// create li
+// add dice to p
+// append to ul
+
+
+// EVENT LISTENERS
+/**
+ * Clicking the roll button the first time should not add anything to the history
+ * Subsequent clicks to the rolls button should add the previous roll to the history
+ * 
+ */
