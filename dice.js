de143fcaaf955e29b70a414290dc14eafb1c3f6e
Marsha Hall <mphall77@gmail.com>
2021-02-28 15:09:42 -0500

    clean up code

--- dice.js
+++ dice.js
@@ -8,10 +8,8 @@ const userInput = document.querySelector("input");
 const diceBtn = document.querySelector("button");
 const pSum = document.querySelector("#sum-para");
 const pDice = document.querySelector("#dice");
-let pTest = document.querySelector("#test");
 let diceList = "";
 let sum = 0;
-let die = document.querySelector("#test");
 
 // HISTORY SELECTORS
 const ul = document.querySelector("ul");
@@ -22,22 +20,17 @@ let clicks = 0;
 // FUNCTIONS
 
 const getDice = (event) => {
-	rolls = userInput.valueAsNumber;
+	let rolls = userInput.valueAsNumber;
 	for (let i = 0; i < rolls; i++) {
         let num = Math.floor(Math.random() * 5) + 1;
-        // die = (diceArr[num]);
-        // die.replace("&#", "");
-        die = diceArr[num].replace("&#", "");
+        let die = diceArr[num].replace("&#", "");
 		diceList += String.fromCharCode(die);
 		sum += Number(diceVal[num]);
-        console.log(die);
 	}
 	
 	pDice.textContent = diceList;
 	pSum.textContent = sum;
 	userInput.value = "";
-
-    pTest.textContent = String.fromCharCode(die);
 
 	const reset = () => {
 		historyLog2 = historyLog;
@@ -45,19 +38,6 @@ const getDice = (event) => {
 		sum = 0;
 		historyLog = "";
 	};
-
-	// const convertDice = () => {
-	// how do I display the dice image?
-	// needs to go into the html w/o the quotes
-	// remove &# and pass the numbers into fromCharCode gives me the dice
-	//  die = (diceArr[num]);
-    // die.replace(/&#/, "");
-
-	// die += diceList.fromCharCode(9856); // null[die]
-	// console.log(die);
-	// diceList += die;
-	// diceList += diceArr[num];
-	// };
 
 	const createHistory = () => {
 		const li = document.createElement("li");
@@ -74,6 +54,8 @@ const getDice = (event) => {
 // EVENT LISTENERS
 diceBtn.addEventListener("click", getDice);
 
+
+
 // RESOURCES
 // https://gomakethings.com/decoding-html-entities-with-vanilla-javascript/
 
@@ -88,3 +70,17 @@ diceBtn.addEventListener("click", getDice);
 var decoded = decodeHTML(
 	"&lt;p&gt;In this course, you&rsquo;ll learn:&lt;/p&gt;"
 ); */
+
+// JUST THINKING
+// const convertDice = () => {
+	// how do I display the dice image?
+	// needs to go into the html w/o the quotes
+	// remove &# and pass the numbers into fromCharCode gives me the dice
+	//  die = (diceArr[num]);
+    // die.replace(/&#/, "");
+
+	// die += diceList.fromCharCode(9856); // null[die image]
+	// console.log(die);
+	// diceList += die;
+	// diceList += diceArr[num];
+	// };
