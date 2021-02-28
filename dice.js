f094ce7177c3a7218c8672ce841f5ae04ad4535f
Marsha Hall <mphall77@gmail.com>
2021-02-28 12:45:39 -0500

    edit unnecessary code

--- dice.js
+++ dice.js
@@ -19,8 +19,6 @@ let clicks = 0;
 
 // FUNCTIONS
 const getDice = (event) => {
-	event.preventDefault();
-
 	rolls = userInput.valueAsNumber;
 	for (let i = 0; i < rolls; i++) {
 		let num = Math.floor(Math.random() * 5) + 1;
@@ -30,53 +28,29 @@ const getDice = (event) => {
 		// convertDice()
 		sum += Number(diceVal[num]);
 	}
-	pDice.textContent = diceList; //need to store this before the second click
+	pDice.textContent = diceList; 
 	pSum.textContent = sum;
 	userInput.value = "";
 
-    const reset = () => {
-        historyLog2 = historyLog;
+	const reset = () => {
+		historyLog2 = historyLog;
 		diceList = "";
 		sum = 0;
 		historyLog = "";
 	};
 
 	const createHistory = () => {
-		// event.preventDefault();
 		const li = document.createElement("li");
 		li.textContent = historyLog2;
 		ul.appendChild(li);
-		// historyLog = "";
-        reset();
+		reset();
 	};
 
 	clicks++;
-	// on click 1 save the (diceList = sum) values
-	// reset diceList
-	// pass current li  to historyLog
-	if (clicks === 1) {
-		historyLog = `${pDice.textContent} = ${sum}`;
-        reset();
-	}
-    if (clicks > 1) {
-        historyLog = `${pDice.textContent} = ${sum}`;
-		createHistory();
-	}
+	historyLog = `${pDice.textContent} = ${sum}`;
+	clicks <= 1 ? reset() : createHistory();
+	
 };
-// get the user input value as a number
-// loop userInput times to generate the dice $ value
-// add to string ==> need a variable
-// add the sum
-// reset input to 0
-
-// create li
-// add dice to p
-// append to ul
 
 // EVENT LISTENERS
-/**
- * Clicking the roll button the first time should not add anything to the history
- * Subsequent clicks to the rolls button should add the previous roll to the history
- *
- */
 diceBtn.addEventListener("click", getDice);
